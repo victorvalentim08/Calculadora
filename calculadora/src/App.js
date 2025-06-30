c51d36988d1101e08667bfa7f37c72e27593b542
Victor Valentim <victorvalentim0803@gmail.com>
2025-06-29 23:21:31 -0300

    Começo calculadora

---
+++ calculadora/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="Calculadora">
+      <div className='display'>
+        <div className='input'></div>
+        <div className='resultado'></div>
+      </div>
+      <div className='buttons'>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
